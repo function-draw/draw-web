51e2c3aa6fe3ae4ac805dd51cdada5e50b7ba9d1
W0NYV <w0nyv.707@gmail.com>
2024-12-14 01:50:54 +0900

    Fix: 一部jsをクライアントで実行するように修正

--- src/lib/tokyo2-bg-gfx.svelte
+++ src/lib/tokyo2-bg-gfx.svelte
@@ -4,19 +4,22 @@
 
     import vertexShader from './tokyo2-bg-gfx.vert?raw';
     import fragmentShader from './tokyo2-bg-gfx.frag?raw'; 
+    import { browser } from "$app/environment";
 
     export let canvas: HTMLCanvasElement;
 
     const getMobileOS = () => {
+        if (!browser) return "";
+
         const ua = navigator.userAgent;
         if (/android/i.test(ua)) {
             return "Android";
         }
-        else if ((/iPad|iPhone|iPod/.test(ua)) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)){
+        else if ((/iPad|iPhone|iPod/.test(navigator.userAgent)) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)){
             return "iOS";
         }
         return "Other";
-    }
+    };
 
     onMount(() => {
 
