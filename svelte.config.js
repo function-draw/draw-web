893e26068276bb5122050f1c8477cdb61f09a7f7
uni101a <hanacchiglon07@gmail.com>
2024-12-15 16:31:01 +0900

    本番ビルド用にSvelte.configを書き換え

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,28 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
+            // default options are shown
             pages: 'build',
             assets: 'build',
             fallback: "./404/index.html",
-            precompress: false,
-            strict: true
-        })
+            precompress: false
+        }),
+	},
+	paths: {
+		// GitHub Pagesのサブディレクトリ対応
+		//base: process.env.NODE_ENV === 'production' ? /draw-web/ : '',
+	},
+	vite: {
+		build: {
+		  outDir: 'build'
+		}
+	},
+	prerender: {
+		default: true
 	}
 };
 
